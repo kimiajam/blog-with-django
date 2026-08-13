05a64ff7fb314cefeeb03eddb9d993a28787852a
kimiajam <kimiajaam@gmail.com>
2026-08-13 12:15:50 +0330

    add a model for the posts

--- website/models.py
+++ website/models.py
@@ -5,4 +5,8 @@ class Contact(models.Model):
     email=models.EmailField()
     subject=models.CharField(max_length=255)
     message=models.TextField()
+
+    def __str__(self):
+        return self.name
+    
     
